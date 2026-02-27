f39a38bee4f4e9c152645290f685a620201d7a74
chacha86 <taejincha0619@gmail.com>
2026-02-27 14:20:46 +0900

    15

--- src/test/java/com/back/QuestionRepositoryTest.java
+++ src/test/java/com/back/QuestionRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -108,5 +109,29 @@ public class QuestionRepositoryTest {
         questionRepository.delete(q1);
 
         assertThat(questionRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("답글 저장")
+    @Transactional
+    void t6() {
+        Question q1 = questionRepository.findById(2).get();
+
+        Answer a1 = new Answer();
+        a1.setContent("답글 1");
+
+//        q1.getAnswerList().add(a1); // q1의 답글 목록에 a1 추가
+//        questionRepository.save(q1);
+
+        a1.setQuestion(q1); // a1이 q1을 참조하도록 설정
+        answerRepository.save(a1);
+        answerRepository.flush();
+
+        Answer foundedAnswer = answerRepository.findById(1).get();
+
+        assertThat(foundedAnswer.getId()).isEqualTo(1);
+        assertThat(foundedAnswer.getContent()).isEqualTo("답글 1");
+
+
     }
 }
